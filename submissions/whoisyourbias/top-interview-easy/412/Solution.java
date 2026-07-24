19dee072e7c6d39afc69bfb6d9990d2809078194
whoisyourbias <rkdaudghks99@gmail.com>
2026-07-24 11:36:11 +0900

    feat

--- submissions/whoisyourbias/top-interview-easy/412/Solution.java
+++ submissions/whoisyourbias/top-interview-easy/412/Solution.java
@@ -5,7 +5,7 @@ class Solution {
 	public List<String> fizzBuzz(int n) {
 		List<String> lst = new LinkedList<>();
 
-		for (int i = 1; i <= 4; i++) {
+		for (int i = 1; i <= n; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				lst.add("FizzBuzz");
 			} else if (i % 3 == 0) {
