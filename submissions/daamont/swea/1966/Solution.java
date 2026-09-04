0d1a2f25cef7d6ffa0a0dcc96650111ca9d184d7
daamont <daamont01@gmail.com>
2026-09-04 15:46:14 +0900

    solve : swea 1966

---
+++ submissions/daamont/swea/1966/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = sc.nextInt();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+
+            System.out.print("#" + test_case);
+            for (int i = 0; i < n; i++) {
+                System.out.print(" " + arr[i]);
+            }
+            System.out.println();
+        }
+    }
+}
