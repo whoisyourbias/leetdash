87d7c8dbef3d44720a7fe3b7e45f89082ba7ddc3
강명환 <rkdaudghks99@gmail.com>
2026-09-10 11:49:19 +0900

    solve: programmers 178870

--- submissions/whoisyourbias/programmers/178870/Solution.java
+++ submissions/whoisyourbias/programmers/178870/Solution.java
@@ -9,49 +9,37 @@ class Solution {
     
     public int[] solution(int[] sequence, int k) {
         ArrayList<KV> lst = new ArrayList<>();
-        int r = sequence.length - 1;
-        int sum = 0;
-        int minLength = Integer.MAX_VALUE;
-        while (r >= 0) {
-            sum += sequence[r];
-            int l =r - 1;
-            while (
-                (r-l <= minLength) &&
-                (l >= 0) &&
-                (sequence[l] + sum < k) 
-            ) {
-                sum += sequence[l];
-                l--;
-            }
-            
+        int l = 0;
+        int r = l + 1;
+        int sum = sequence[l];
+        while (l <= r) {
             if (sum == k) {
-                lst.add(new KV(0 ,r,r));
-                r--;
-                sum =0;
-                minLength = Math.min(minLength, 0);
-                continue;
+                lst.add(new KV(r-l, l, r-1));
+                l=r;
+                r=l+1;
+                if (l == sequence.length)
+                    break;
+                sum=sequence[l];
+            } else if (sum < k) {
+                if (r == sequence.length)
+                    break;
+                sum += sequence[r];
+                r += 1;
+            } else {
+                if (l == sequence.length)
+                    break;
+                
+                sum -= sequence[l];
+                l++;
             }
-            
-            if (l >= 0 &&
-                sequence[l] + sum == k) {
-                // add answer set;
-                lst.add(new KV(r-l,l,r));
-                minLength = Math.min(minLength, r-l);
-                r--;
-                sum =0;
-                continue;
-            }
-            sum = 0;
-            r--;
         }
         
-        int[] answer = new int[2];
-        
-        Collections.sort(lst, (a, b) -> {
+        Collections.sort(lst, (a,b) -> {
             if (a.length == b.length)
                 return a.l - b.l;
             return a.length - b.length;
         });
+        int[] answer = new int[2];
         answer[0] = lst.get(0).l;
         answer[1] = lst.get(0).r;
         return answer;
