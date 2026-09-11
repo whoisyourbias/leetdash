360124de0e69db83b14d1e2f4eb8d465c53294b8
강명환 <rkdaudghks99@gmail.com>
2026-09-11 09:17:31 +0900

    solve: programmers 42883

---
+++ submissions/whoisyourbias/programmers/42883/Solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        boolean[] selected = new boolean[number.length()];
+        Arrays.fill(selected, true);
+        int i =0;
+        while (i < number.length()) {
+            int before = i;
+            
+            int max = -1;
+            int max_i = -1;
+            for (; (i < before + k + 1) && (i < number.length()); i++) {
+                if (number.charAt(i) - '0' > max) {
+                    max = number.charAt(i) - '0';
+                    max_i = i;
+                }
+            }
+            i = before;
+            // System.out.println(max + " " + max_i);
+            // System.out.println(i + " " + k);
+            while (k > 0 && i < max_i) {
+                selected[i] = false;
+                k--;
+                i++;
+            }
+            
+            if (k == number.length() - i - 1) {
+                i++;
+                while (i < number.length()) {
+                   selected[i++] = false;
+                    k--;
+                }
+            }
+            
+            i++;
+        }
+        
+        for (i = 0; i < number.length(); i++) {
+            if (selected[i] == true)
+                answer = answer.concat(String.valueOf(number.charAt(i)));
+        }
+        return answer;
+    }
+}
