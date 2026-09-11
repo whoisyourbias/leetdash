ea74f63e91abc5efa543d39b68cef9d4179d48cf
강명환 <rkdaudghks99@gmail.com>
2026-09-11 10:20:40 +0900

    solve: programmers 42883

--- submissions/whoisyourbias/programmers/42883/Solution.java
+++ submissions/whoisyourbias/programmers/42883/Solution.java
@@ -2,43 +2,31 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        String answer = "";
-        boolean[] selected = new boolean[number.length()];
-        Arrays.fill(selected, true);
-        int i =0;
-        while (i < number.length()) {
-            int before = i;
-            
-            int max = -1;
-            int max_i = -1;
-            for (; (i < before + k + 1) && (i < number.length()); i++) {
-                if (number.charAt(i) - '0' > max) {
-                    max = number.charAt(i) - '0';
-                    max_i = i;
-                }
+        StringBuilder stack = new StringBuilder();
+        
+        for (int i = 0; i < number.length() ; i++) {
+            if (stack.isEmpty()) {
+                stack.append(number.charAt(i));
+                continue;
             }
-            i = before;
-            while (k > 0 && i < max_i) {
-                selected[i] = false;
+            
+            while (
+                k > 0 &&
+                stack.length() > 0 &&
+                stack.charAt(stack.length() -1) < number.charAt(i)
+            ) {
+                stack.deleteCharAt(stack.length() - 1);
                 k--;
-                i++;
             }
             
-            if (k == number.length() - i - 1) {
-                i++;
-                while (i < number.length()) {
-                   selected[i++] = false;
-                    k--;
-                }
-            }
-            
-            i++;
+            stack.append(number.charAt(i));
         }
         
-        for (i = 0; i < number.length(); i++) {
-            if (selected[i] == true)
-                answer = answer.concat(String.valueOf(number.charAt(i)));
+        while (k > 0) {
+            stack.deleteCharAt(stack.length() - 1);
+            k--;
         }
-        return answer;
+        
+        return stack.toString();
     }
 }
