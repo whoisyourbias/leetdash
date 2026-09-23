4820005aad28e40ec633eaa1b3688b7efbf79160
강명환 <rkdaudghks99@gmail.com>
2026-09-23 15:08:51 +0900

    solve: swea 5215

--- submissions/whoisyourbias/swea/5215/Solution.java
+++ submissions/whoisyourbias/swea/5215/Solution.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 import java.io.FileInputStream;
 
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
 class Solution
 {
     static class Ingre {
