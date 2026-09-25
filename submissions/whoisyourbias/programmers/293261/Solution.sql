5dd7a082145e743eacbe856c03e861c39c17f347
강명환 <rkdaudghks99@gmail.com>
2026-09-25 20:37:51 +0900

    solve: programmers 293261

--- submissions/whoisyourbias/programmers/293261/Solution.sql
+++ submissions/whoisyourbias/programmers/293261/Solution.sql
@@ -1,9 +1,12 @@
-select fi.ID, fni.FISH_NAME, fi.LENGTH
+select fi.ID, fni.FISH_NAME, m.max_length LENGTH
 from fish_info fi
 left join fish_name_info fni
 on fi.fish_type = fni.fish_type
-where fi.length = (
-    select max(fii.length) from fish_info fii
-    where fii.fish_type = fi.fish_type
-)
+ join (
+    select max(length) max_length, fish_type
+    from fish_info
+    group by fish_type
+) m
+on m.fish_type = fi.fish_type
+and fi.length = m.max_length
 order by fi.id asc;
